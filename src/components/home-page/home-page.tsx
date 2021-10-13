f48ab921ca3ae079df13a5e010cf80aaf1460055
Jeremy Orme <jeremy@heobe.com>
2021-10-13 10:52:34 +0100

    fix(home-page): update sub-heading text

--- src/components/home-page/home-page.tsx
+++ src/components/home-page/home-page.tsx
@@ -18,7 +18,8 @@ export class HomePage {
         </navbar-block>
         <header-block>
           <h1 slot="left">Shop local. Help <strong>Farnborough</strong> thrive!</h1>
-          <p slot="left">The high-street is the beating heart of our local community and vibrant small business underpins our freedom and democracy. Support our local businesses for a stronger community and brighter future.</p>
+          <p slot="left">The high-street is the beating heart of our local community and vibrant small business underpins our freedom and democracy.</p>
+          <p slot="left"><strong>Support our local businesses</strong> for a stronger community and brighter future.</p>
           <ion-button slot="left" href="#/shopping/" strong={true}>Find a Shop</ion-button>
           <map-block id="local-map" slot="right" latitude={51.2869} longitude={-0.7526} zoom={13}/>
         </header-block>
