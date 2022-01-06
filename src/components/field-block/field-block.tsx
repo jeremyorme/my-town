2b18016e5e33a1b904541d4c4dce2ff8de6e7c40
Jeremy Orme <jeremy@heobe.com>
2022-01-06 17:54:06 +0000

    fix(field-block): field buttons shrinking

--- src/components/field-block/field-block.tsx
+++ src/components/field-block/field-block.tsx
@@ -23,13 +23,13 @@ export class FieldBlock {
       <Host>
         {this.isEditing ? <div class="field">
           <span class="content editing" contenteditable={true} ref={el => this.textInput = el as HTMLElement}>{this.value}</span>
-          <ion-icon class="editing-icon" name="checkmark-done-outline" size={this.iconSize} onClick={() => {this.valueChanged.emit(this.textInput.innerText); this.isEditing = false;}}/>
-          <ion-icon class="editing-icon" name="close-outline" size={this.iconSize} onClick={() => {this.isEditing = false}}/>
+          <div class="editing-icon"><ion-icon name="checkmark-done-outline" size={this.iconSize} onClick={() => {this.valueChanged.emit(this.textInput.innerText); this.isEditing = false;}}/></div>
+          <div class="editing-icon"><ion-icon name="close-outline" size={this.iconSize} onClick={() => {this.isEditing = false}}/></div>
         </div> : <div class={'field' + (this.loading ? ' loading' : '')}>
           {this.isLink ?
             <a href={this.value} target="_blank">{this.value}</a> :
             <div class="content">{this.value.split('\n').map((p, j) => <span>{j != 0 ? <br/> : null}{p.split('*').map((s, i) => s.length == 0 ? null : i % 2 == 0 ? <span>{s}</span> : <strong>{s}</strong>)}</span>)}</div>}
-          {!this.readOnly && !this.loading ? <ion-icon class="editing-icon" name="create-outline" size={this.iconSize} onClick={() => {this.isEditing = true}}/> : null}
+          {!this.readOnly && !this.loading ? <div class="editing-icon"><ion-icon name="create-outline" size={this.iconSize} onClick={() => {this.isEditing = true}}/></div> : null}
         </div>}
       </Host>
     );
