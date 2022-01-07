6a621d7688f6169bedbf21eaf4e9f4a7a0c27779
Jeremy Orme <jeremy@heobe.com>
2022-01-07 10:46:08 +0000

    fix(field-block): remove excess spacing around icons

--- src/components/field-block/field-block.tsx
+++ src/components/field-block/field-block.tsx
@@ -21,11 +21,11 @@ export class FieldBlock {
   render() {
     return (
       <Host>
-        {this.isEditing ? <div class="field">
+        {this.isEditing ? <div class={'field ' + (this.iconSize == 'large' ? 'large-icons' : 'small-icons')}>
           <span class="content editing" contenteditable={true} ref={el => this.textInput = el as HTMLElement}>{this.value}</span>
           <div class="editing-icon"><ion-icon name="checkmark-done-outline" size={this.iconSize} onClick={() => {this.valueChanged.emit(this.textInput.innerText); this.isEditing = false;}}/></div>
           <div class="editing-icon"><ion-icon name="close-outline" size={this.iconSize} onClick={() => {this.isEditing = false}}/></div>
-        </div> : <div class={'field' + (this.loading ? ' loading' : '')}>
+        </div> : <div class={'field ' + (this.iconSize == 'large' ? 'large-icons' : 'small-icons') + (this.loading ? ' loading' : '')}>
           {this.isLink ?
             <a href={this.value} target="_blank">{this.value}</a> :
             <div class="content">{this.value.split('\n').map((p, j) => <span>{j != 0 ? <br/> : null}{p.split('*').map((s, i) => s.length == 0 ? null : i % 2 == 0 ? <span>{s}</span> : <strong>{s}</strong>)}</span>)}</div>}
